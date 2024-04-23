78fb7b8f0b6d88ad6dfe0004f2ff9de0692feb54
Post-Enot <ve.vyshegorodsky.cold@gmail.com>
2024-04-24 01:41:40 +0300

    Создан класс IUP Character Controller
    IUP Character Controller является надстройкой над классическим Character Controller от Unity - он реализует постоянное обновление положения, обработку физики, гравитации, возможность реализации механики прыжка. IUP Character Controller взаимодействует с классическим Unity Character Controller.
    
    Название IUP_CharacterController имеет префикс для совместимости с Unity - без префикса IUP_ операции добавления, взятия компонента и пр. могут некорректно работать

---
+++ Children of the Deep/Assets/Scripts/C#/Character Controller/IUP_CharacterController.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+
+namespace IUP.ChildrenOfTheDeep
+{
+    [RequireComponent(typeof(CharacterController))]
+    public sealed class IUP_CharacterController : MonoBehaviour
+    {
+        [field: SerializeField] public float Gravity { get; private set; } = Physics.gravity.y;
+
+        public Vector3 Velocity => _velocity;
+        public bool IsGrounded => _characterController.isGrounded;
+
+        private CharacterController _characterController;
+        private Vector3 _velocity;
+
+        public void SetHorizontalMovement(Vector2 speed)
+        {
+            _velocity = new Vector3(speed.x, Velocity.y, speed.y);
+        }
+
+        public void StopHorizontalMovement()
+        {
+            _velocity = new Vector3(0.0f, Velocity.y, 0.0f);
+        }
+
+        public void ApplyVerticalMovement(float velocity)
+        {
+            _velocity.y += velocity;
+        }
+
+        private void Awake()
+        {
+            _characterController = GetComponent<CharacterController>();
+        }
+
+        private void FixedUpdate()
+        {
+            ApplyGravity();
+            ApplyMotion();
+            ResetVerticalVelocityIfGrounded();
+        }
+
+        private void ApplyGravity()
+        {
+            _velocity.y += Gravity * Time.fixedDeltaTime;
+        }
+
+        private void ApplyMotion()
+        {
+            Vector3 motion = _velocity * Time.fixedDeltaTime;
+            _characterController.Move(motion);
+        }
+
+        private void ResetVerticalVelocityIfGrounded()
+        {
+            if (IsGrounded)
+            {
+                _velocity.y = 0.0f;
+            }
+        }
+    }
+}
