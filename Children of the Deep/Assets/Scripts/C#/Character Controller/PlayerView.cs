ae17b0039ed53c2a33fdfcb74eaa7e3d7671db3a
Post-Enot <ve.vyshegorodsky.cold@gmail.com>
2024-05-04 19:37:53 +0300

    Добавлен временный скрипт представления игрока
    + Добавлен скрипт PlayerView для переключения анимаций: скрипт берёт данные из других компонентов игрока и передаёт в контроллер анимаций
    
    Данный скрипт сейчас видится скорее временной заглушкой, и кажется не лучшим решением с точки зрения архитектуры.

---
+++ Children of the Deep/Assets/Scripts/C#/Character Controller/PlayerView.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+namespace IUP.ChildrenOfTheDeep
+{
+    [RequireComponent(typeof(Animator))]
+    public sealed class PlayerView : MonoBehaviour
+    {
+        [SerializeField] private string _rotationY_ParamName;
+        [SerializeField] private string _isMoveParamName;
+        [SerializeField] private string _isRunParamName;
+
+        [Header("Component References:")]
+        [SerializeField] private IUP_CharacterController _characterController;
+        [SerializeField] private PlayerController _playerController;
+
+        private Animator _animator;
+
+        private void Awake()
+        {
+            _animator = GetComponent<Animator>();
+        }
+
+        private void Update()
+        {
+            UpdateRotationY();
+            UpdateIsMove();
+            UpdateIsRun();
+        }
+
+        private void UpdateRotationY()
+        {
+            _animator.SetFloat(_rotationY_ParamName, _characterController.RotationY);
+        }
+
+        private void UpdateIsMove()
+        {
+            _animator.SetBool(_isMoveParamName, _characterController.HasHorizontalMovement);
+        }
+
+        private void UpdateIsRun()
+        {
+            bool isRun = _playerController.ActualMovementMode == MovementMode.Run;
+            _animator.SetBool(_isRunParamName, isRun);
+        }
+    }
+}
