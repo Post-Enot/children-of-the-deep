c1f974552a09b23c3a583f38b54548a4c9b203e1
Post-Enot <ve.vyshegorodsky.cold@gmail.com>
2024-05-06 09:11:10 +0300

    Добавлены скрипты для простейшей реализации FSM
    + Создана папка Assets\Scripts\C#\State Graph
    + Созданы два скрипта: StateGraph и State для простейшей реализации машин состояний

---
+++ Children of the Deep/Assets/Scripts/C#/State Graph/State.cs
@@ -0,0 +1,11 @@
+﻿namespace IUP.ChildrenOfTheDeep
+{
+    public abstract class State
+    {
+        public virtual void OnEnter() { }
+
+        public virtual void OnUpdate() { }
+
+        public virtual void OnExit() { }
+    }
+}
---
+++ Children of the Deep/Assets/Scripts/C#/State Graph/StateGraph.cs
@@ -0,0 +1,25 @@
+﻿namespace IUP.ChildrenOfTheDeep
+{
+    public class StateGraph
+    {
+        public StateGraph(State state)
+        {
+            CurrentState = state;
+            CurrentState.OnEnter();
+        }
+
+        public State CurrentState { get; private set; }
+
+        public void TransitTo(State state)
+        {
+            CurrentState.OnExit();
+            CurrentState = state;
+            CurrentState.OnEnter();
+        }
+
+        public void Update()
+        {
+            CurrentState.OnUpdate();
+        }
+    }
+}
