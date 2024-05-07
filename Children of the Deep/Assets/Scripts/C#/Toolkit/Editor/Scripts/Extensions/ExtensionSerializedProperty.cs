46a42f49cd3bd1d4729c592f20b0b9558a66af24
Post-Enot <ve.vyshegorodsky.cold@gmail.com>
2024-05-07 08:44:58 +0300

    Создана основа IUP Toolkit
     + из /.gitignore удалено игнорирование папки и меты /Assets/Scripts/Toolkit/
     + Создана папка IUP Toolkit, в которую будут добавляться общие утилиты и полезные скрипты; в дальнейшем на основе её содержимого планируется создать отдельный пакет
     + Созданы две сборки IUP Toolkit: для редактора и для рантайма соотвественно
     + Созданы 4 класса-обёртки для сериализации словарей: SK_SV_Dictionary, SK_SRV_Dictionary, SRK_SV_Dictionary, SRK_SRV_Dictionary, каждый из которых в зависимости от префикса сохраняет ключи и значения по значению и по ссылке соотвественно
     + Создан статический класс-расширение ExtensionSerializedProperty с двумя методами: InsertArrayElementAt и InserArrayElementAtE, расширяющие возможности модификации коллекций с помощью обычного SerializedProperty

---
+++ Children of the Deep/Assets/Scripts/C#/Toolkit/Editor/Scripts/Extensions/ExtensionSerializedProperty.cs
@@ -0,0 +1,20 @@
+﻿using UnityEditor;
+
+namespace IUP.Toolkit.Editor
+{
+    public static class ExtensionSerializedProperty
+    {
+        public static SerializedProperty InsertArrayElementAt(this SerializedProperty @this, int index)
+        {
+            @this.InsertArrayElementAtIndex(index);
+            return @this.GetArrayElementAtIndex(index);
+        }
+
+        public static SerializedProperty InsertArrayElemenetAtEnd(this SerializedProperty @this)
+        {
+            int index = @this.arraySize;
+            @this.InsertArrayElementAtIndex(index);
+            return @this.GetArrayElementAtIndex(index);
+        }
+    }
+}
---
+++ Children of the Deep/Assets/Scripts/C#/Toolkit/Runtime/Scripts/SDictionary/SK_SV_Dictionary.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace IUP.Toolkit.SerializableCollections
+{
+    public class SK_SV_Dictionary<TKey, TValue>
+    {
+        [Serializable]
+        public struct SKeyValuePair
+        {
+            public SKeyValuePair(TKey key, TValue value)
+            {
+                Key = key;
+                Value = value;
+            }
+
+            public static readonly string KeyPropertyPath = nameof(Key);
+            public static readonly string ValuePropertyPath = nameof(Value);
+
+            [SerializeField] public TKey Key;
+            [SerializeField] public TValue Value;
+        }
+
+        public static readonly string SKeyValuePairsPropertyPath = nameof(_sKeyValuePairs);
+
+        public Dictionary<TKey, TValue> Dictionary { get; set; } = new();
+
+        [SerializeField] private List<SKeyValuePair> _sKeyValuePairs;
+
+        public void OnAfterDeserialize()
+        {
+            Dictionary = new Dictionary<TKey, TValue>(_sKeyValuePairs.Count);
+            foreach (SKeyValuePair sKeyValuePair in _sKeyValuePairs)
+            {
+                Dictionary.Add(sKeyValuePair.Key, sKeyValuePair.Value);
+            }
+        }
+
+        public void OnBeforeSerialize()
+        {
+            _sKeyValuePairs = new List<SKeyValuePair>(Dictionary.Count);
+            foreach (KeyValuePair<TKey, TValue> keyValuePair in Dictionary)
+            {
+                SKeyValuePair sKeyValuePair = new(keyValuePair.Key, keyValuePair.Value);
+                _sKeyValuePairs.Add(sKeyValuePair);
+            }
+        }
+    }
+}
