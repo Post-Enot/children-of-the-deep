d0f7625caeb1cc76f7876022c684b14f28682bec
Post-Enot <ve.vyshegorodsky.cold@gmail.com>
2024-05-13 09:36:15 +0300

    Добавлена оболочка над PopupWindowContent
     + Добавлена оболочка над PopupWindowContent, облегчающая создание нового содержимого для popup-окон

---
+++ Children of the Deep/Assets/Scripts/C#/Toolkit/Editor/Scripts/PopupWindowContent.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace IUP.Toolkit.Editor
+{
+    public abstract class PopupWindowContent : UnityEditor.PopupWindowContent
+    {
+        public Vector2 WindowSize { get; set; }
+
+        public override Vector2 GetWindowSize() => WindowSize;
+
+        public override void OnGUI(Rect rect) { }
+    }
+}
